70650b76249f5c64561e3bb414bfafc92b45d198
liu66 <2093548876@qq.com>
2024-09-02 20:42:40 +0800

    解决了node.js返回数据时会有的延迟问题和数据错误问题

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const require = createRequire(import.meta.url);
 const express = require('express')
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 // 数据库连接对象
 const client = mysql.createConnection({
@@ -17,22 +17,38 @@ const client = mysql.createConnection({
     password: '209354',
     database: 'node_db',
 })
-
+let Sql_result
 // 数据库查询函数
-function Sql_func(sqlmessage) {
-    client.query(sqlmessage, (err, res) => {
-        if (err) {
-            throw error;
-        }
-        console.log("--sqlmessage--", JSON.stringify(res));
-        return JSON.parse(JSON.stringify(res))
+function Sql_func(sql) {
+    return new Promise((resolve, reject) => {
+        client.query(sql, (err, res) => {
+            if (err)
+                reject("执行sql查询失败")
+            console.log("--sql_result--", JSON.parse(JSON.stringify(res)));
+            resolve(JSON.parse(JSON.stringify(res)))
+        })
     })
 }
 
 app.get('/test', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*') // *表示都可以跨域访问
-    Sql_func('select * from user')
+
+    // 拿取返回结果时需要注意异步操作
+    let result = Sql_func('select * from user').then((result) => {
+        res.send(result)
+    })
 })
+
+app.get('/test2', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*') // *表示都可以跨域访问
+    Sql_func2('select * from user').then((result) => {
+        console.log("result", result);
+        res.send(result)
+    })
+
+})
+
+
 app.listen(8899, () => {
     console.log("后台服务开启")
 })
